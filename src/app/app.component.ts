225edf276108bf290f448f23345849c2a4e5adae
Ufuk Arslan <ufuk.arslan@hes-so.ch>
2021-11-22 13:14:02 +0100

    firstCommit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angularBlankProject';
+  title: string;
+  titleComment: string;
+  greeting: string;
+  titleClasses: { [ name: string ]: boolean };
+
+  constructor() {
+    this.title = 'angular-starter';
+    this.titleComment = 'This is awesome!';
+    this.greeting = '';
+    this.titleClasses = {
+      italic: true,
+      hoverable: true
+    };
+  }
+
+  onTitleClicked(event: MouseEvent) {
+    console.log('The title was clicked', event);
+  }
+
+  hello(name: string): string {
+    return `Hello ${name}`;
+  }
+
 }
